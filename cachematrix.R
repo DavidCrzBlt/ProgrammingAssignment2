0676d866fc46edaca74c8111551b3a2f04db7680
DavidCrzBlt <cruzdavid158@gmail.com>
2020-10-29 17:57:29 -0600

    Description of the functions.
    Program that calculates the inverse of a atrix but if the same matrix needs to be calculated again it will not do it. Instead of that, it will search for the previous result in the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{ 
+  m <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse() #Search for the result
+  if(!is.null(m))	#If the result was calculated before
+  {
+    message("getting cached data")
+    return(m)
+  }
+  #If the inverse is not found then it will calculate it
+  data <- x$get()	#Search for the matrix
+  m <- solve(data, ...)	#Execute solve function
+  x$setinverse(m)		#Save result 
+  m				#Show result
 }
